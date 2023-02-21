b723c83f0abaf499238deee752aafde6bcd895c8
Akhilendra <akhilendra1604@gmail.com>
2023-02-21 23:17:49 +0530

    added: landing

---
+++ src/components/UI/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '@mui/material';
+// import './App.css';
+import './Home.css';
+import { makeStyles } from '@material-ui/styles';
+import { Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import classes from './Header.module.css';
+
+import './Header.css';
+export const App = () => {
+    return (
+        <div className="container_">
+            
+        </div>
+    );
+};
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Nav1 from '../components/UI/Nav1';
 import About from '../components/About';
 import Sponsors from '../components/UI/Sponsors';
-
 import Footer from '../components/UI/Footer';
+import Header from '../components/UI/Header'
 
 const Home = () => {
 	return (
 		<div>
 			<Nav1></Nav1>
+			<Header></Header>
 			<About></About>
-			<Sponsors></Sponsors>
+			{/* <Sponsors></Sponsors> */}
 			<Footer></Footer>
 		</div>
 	);
