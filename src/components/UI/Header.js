d65e130cbe2b9db9edc8190f377595efe9f66d3b
jake <jjk848798@gmail.com>
2023-02-23 16:53:18 +0530

    fix:footer

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 // import './App.css';
 import './Home.css';
 import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { motion } from 'framer-motion';
 import classes from './Header.module.css';
 import logo from '../../assets/tedxmanit.png';
 import ISTE from '../../assets/ISTElogo.svg';
-
+import { HashLink } from 'react-router-hash-link';
 import './Header.css';
 export const App = () => {
-    return (
-        <div className="container_">
-            <div className="logos">
-                <img src={ISTE} className='iste'/>
-                <img src={logo} className='tedx'/>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container_">
+			<div className="logos">
+				<img src={ISTE} className="iste" />
+				<img src={logo} className="tedx" />
+				<Box display="flex" gap={'15px'}>
+					<Button
+						variant="contained"
+						sx={{ background: 'red' }}
+						disableElevation
+					>
+						{' '}
+						Register Now!
+					</Button>
+					<Button
+						sx={{ background: 'black', color: 'red', border: 'red' }}
+						variant="outlined"
+					>
+						<HashLink
+							to={'#about'}
+							smooth
+							style={{ textDecoration: 'none', color: 'red' }}
+						>
+							Read More
+						</HashLink>
+					</Button>
+				</Box>
+			</div>
+		</div>
+	);
 };
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,13 @@ import Nav1 from '../components/UI/Nav1';
 import About from '../components/About';
 import Sponsors from '../components/UI/Sponsors';
 import Footer from '../components/UI/Footer';
-import Header from '../components/UI/Header'
+import Header from '../components/UI/Header';
 import Speakers from '../components/Speakers';
 const Home = () => {
 	return (
 		<div>
 			<Nav1></Nav1>
+
 			<Header></Header>
 			<About></About>
 			<Speakers></Speakers>
